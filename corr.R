872fa9527c12aa986a341f20c2794e528d819781
Daniel <daniel.jungen@t-online.de>
2017-09-05 08:07:19 +0200

    Add files via upload

--- corr.R
+++ corr.R
@@ -7,7 +7,15 @@ corr <- function(directory, threshold=0){
   
   alldata<-read.csv(dateien[1])
   gute<-alldata[!is.na(alldata[,2]) & !is.na(alldata[,3]),]
-  ergebnis<-data.frame(gute)
+  
+  ergebnis<-data.frame()
+  
+  if (dim(gute)[1]> threshold){
+    
+    ergebnis <- cor(gute[,2], gute[,3])
+  }
+  
+  #ergebnis<-data.frame(gute)
   
   if(length(dateien)>1){
   
@@ -15,7 +23,12 @@ corr <- function(directory, threshold=0){
         alldata<-read.csv(dateien[i])
         gute<-alldata[!is.na(alldata[,2]) & !is.na(alldata[,3]),] 
         
-        ergebnis<-rbind(ergebnis,data.frame(gute))
+        if (dim(gute)[1]> threshold){
+          
+          ergebnis<-rbind(ergebnis,cor(gute[,2], gute[,3]))
+        }
+        
+        
       }
   }
 
